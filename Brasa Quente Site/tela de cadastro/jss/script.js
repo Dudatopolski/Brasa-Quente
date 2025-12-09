876b2858ebf0c23b46bc6c425c698dab589c09f4
Dudatopolski <dudatopolski@gmail.com>
2025-12-09 13:35:18 +0000

    salvando alterações

---
+++ Brasa Quente Site/tela de cadastro/jss/script.js
@@ -0,0 +1,10 @@
+function login(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email-input").value;
+  const cpf = document.getElementById("cpf-input").value;
+  const nome = document.getElementById("nome-input").value;
+
+    alert("Login feito com sucesso!");
+    
+}
